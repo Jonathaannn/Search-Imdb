ca004d65ac9d14a12126672b304390c29fdadd0e
Jonathan César <jonathanjog237@gmail.com>
2023-03-29 08:01:36 -0300

    Adicionando lógica dos botões da aplicação

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+search = () => {
+    let title = document.getElementById("titleMovie").value
+    if(!title) {
+       return window.alert("Campo vázio!")
+    }
+    window.location.href = `/schImdb/search/${title}`
+}
+
+info = (imdbID) => {
+    window.location.href = `/schImdb/info/${imdbID}`
+}
+
+backHome = () => {
+    window.location.href = '/schImdb/'
+}
+
+back = () => {
+    window.history.back()
+}
