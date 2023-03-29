76c75fa6d5fc9491f69c9f408b5dcc5b33d61434
Jonathan César <jonathanjog237@gmail.com>
2023-03-29 08:07:55 -0300

    Criando lógica e rota de pesquisa de filmes

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -15,4 +15,26 @@ const home = async (req, res) => {
     }
 }
 
-module.exports = { home }
+const searchMovie = async (req, res) => {
+    const title = req.params.title
+    const url = `http://www.omdbapi.com/?apikey=6c5b1285&s=${title}`
+    try {
+        const moviesList = await axios.get(url)
+        const imdbs = moviesList.data.Search
+        const list = []
+        const movies = []
+        imdbs.forEach(element => {
+            list.push(axios.get(`http://www.omdbapi.com/?apikey=6c5b1285&i=${element.imdbID}`))
+        });
+        const movie = await Promise.all(list)
+        movie.map(element => {
+            movies.push(element.data)
+        })
+        res.render('movies',{movies})
+    } catch (error) {
+        console.log(`Erro: ${error}`)
+        res.status(400).json({Error: "Informação solicitada está incorreta!"})
+    }
+}
+
+module.exports = { home, searchMovie }
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,5 +3,6 @@ const routes = Router()
 const controller = require("../controller/controller")
 
 routes.get('/', controller.home)
+routes.get('/search/:title', controller.searchMovie)
 
 module.exports = routes
